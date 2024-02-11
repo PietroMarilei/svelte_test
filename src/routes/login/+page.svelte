8853ea8753f8f14b7bc4452adb64e9e48e562132
PietroMarilei <pietromrl0@gmail.com>
2024-02-11 16:13:29 +0100

    //add google button

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -31,100 +31,66 @@ import Layout from "$lib/+layout.svelte";
       console.error("Errore durante il login:", response.statusText);
     }
   }
+   function redirectToGoogleLogin() {
+    window.location.href = "http://localhost:3000/auth/google";
+  }
 </script>
 
 <main>
-  
   <Layout>
     <div class="container">
       <div class="col d-flex justify-content-center">
-
-         <form on:submit|preventDefault={handleLogin} 
-    style="width: 22rem;" >
-        <!-- Email input -->
-        <div class="form-outline mb-4 ">
-          <div class="d-flex justify-content-center">
-<label class="form-label text-center" for="form2Example1">Username</label>
+        <form on:submit|preventDefault={handleLogin} style="width: 22rem;">
+          <!-- Email input -->
+          <div class="form-outline mb-4 ">
+            <div class="d-flex justify-content-center">
+              <label class="form-label text-center" for="form2Example1">Username</label>
+            </div>
+            <input type="text" id="form2Example1" class="form-control" bind:value={username} required>
           </div>
-          <input type="text" id="form2Example1" class="form-control" bind:value={username} required>
-          
-        </div>
 
-        <!-- Password input -->
-        <div class="form-outline mb-4">
-          <div class="d-flex justify-content-center">
-<label class="form-label text-center" for="form2Example2">Password</label>
+          <!-- Password input -->
+          <div class="form-outline mb-4">
+            <div class="d-flex justify-content-center">
+              <label class="form-label text-center" for="form2Example2">Password</label>
+            </div>
+            <input type="password" id="form2Example2" class="form-control" bind:value={password} required>
           </div>
-          <input type="password" id="form2Example2" class="form-control" bind:value={password} required>
 
-        </div>
-
-        <!-- 2 column grid layout for inline styling -->
-        <div class="row mb-4">
-          <div class="col d-flex justify-content-center">
-            <!-- Checkbox -->
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked="">
-              <label class="form-check-label" for="form2Example31"> Remember me </label>
+          <!-- 2 column grid layout for inline styling -->
+          <div class="row mb-4">
+            <div class="col d-flex justify-content-center">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked="">
+                <label class="form-check-label" for="form2Example31"> Remember me </label>
+              </div>
+            </div>
+            <div class="col">
+              <a href="#!">Forgot password?</a>
             </div>
           </div>
 
-          <div class="col">
-            <!-- Simple link -->
-            <a href="#!">Forgot password?</a>
+          <!-- Submit button -->
+          <div class="d-flex justify-content-center">
+            <button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
           </div>
-        </div>
 
-        <!-- Submit button -->
-        <div  class="d-flex justify-content-center">
-
-          <button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
-        </div>
-
-        <!-- Register buttons -->
-        <div class="text-center">
-          <p>Not a member? <a href="#!">Register</a></p>
-          <p>or sign up with:</p>
-          <button type="button" class="btn btn-link btn-floating mx-1">
-            <i class="fab fa-facebook-f"></i>
-          </button>
-
-          <button type="button" class="btn btn-link btn-floating mx-1">
-            <i class="fab fa-google"></i>
-          </button>
-
-          <button type="button" class="btn btn-link btn-floating mx-1">
-            <i class="fab fa-twitter"></i>
-          </button>
-
-          <button type="button" class="btn btn-link btn-floating mx-1">
-            <i class="fab fa-github"></i>
-          </button>
-        </div>
-      </form>
-
+          <!-- Register buttons -->
+          <div class="text-center">
+            <p>Not a member? <a href="#!">Register</a></p>
+            <p>or sign up with:</p>
+            <button type="button" class="btn btn-link btn-floating mx-1" on:click={redirectToGoogleLogin}>
+              <i class="fab fa-google"></i> Sign in with Google
+            </button>
+          </div>
+        </form>
       </div>
     </div>
-   
-   <!-- <h1 class="text-center">Login</h1> -->
-  <!-- <form on:submit|preventDefault={handleLogin}>
-    <label for="username">Username:</label>
-    <input type="text" id="username" bind:value={username} required />
 
-    <label for="password">Password:</label>
-    <input type="password" id="password" bind:value={password} required />
-
-    <button type="submit">Login</button>
-  </form> -->
-  
-  
-
-  {#if success}
-    <div>logged in -> </div>
-  {/if}
+    {#if success}
+      <div>logged in -> </div>
+    {/if}
   </Layout>
-
-  
 </main>
 
 <style lang="scss">
